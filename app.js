2e17ffbdcee6a7c66238397b2f75bb35eefc392a
jiahnkim230502 <locker9417@gmail.com>
2023-08-18 22:04:33 +0900

    feature: 회원가입, 로그인, 로그아웃 구현

--- app.js
+++ app.js
@@ -25,4 +25,4 @@ export class ExpressApp {
       });
     });
   };
-}
+};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,4 +5,8 @@ const router = Router();
 
 const userController = new UserController();
 
+router.post("/signup", userController.register);
+router.post("/login", userController.login);
+router.post("/logout", userController.logoutUser);
+
 export default router;
