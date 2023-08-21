9411efc5107e5c7843d2d67b960327691022c442
BAE BYEONGIL <b000202@naver.com>
2023-08-21 10:37:51 +0900

    feat: schedule

--- repositories/schedule.repository.js
+++ repositories/schedule.repository.js
@@ -1,5 +1,50 @@
 import { Schedule } from '../db';
 
-class ScheduleRepository {}
+class ScheduleRepository {
+  postSchedule = async (title, date) => {
+    try {
+      const result = await Schedule.create({
+        title,
+        date,
+      });
+
+      return result;
+    } catch (err) {}
+  };
+
+  oneGetSchedule = async scheduleId => {
+    try {
+      const result = await Schedule.findOne({
+        where: { scheduleId },
+      });
+
+      return result;
+    } catch (err) {}
+  };
+
+  putSchedule = async (scheduleId, title, date) => {
+    try {
+      const result = await Schedule.update(
+        {
+          title,
+          date,
+        },
+        { where: { scheduleId } }
+      );
+
+      return result;
+    } catch (err) {}
+  };
+
+  deleteSchedule = async scheduleId => {
+    try {
+      const result = await Schedule.destroy({
+        where: { scheduleId },
+      });
+
+      return result;
+    } catch (err) {}
+  };
+}
 
 export default ScheduleRepository;
