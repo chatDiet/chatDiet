e4c2fcb67ef7aaf9f2e9f24c25d50de2a0c3a4a3
hyem00 <hemi1738@naver.com>
2023-08-28 11:17:19 +0900

    백엔드 최종본 + 프론트 틀 합친 버전

--- services/company.service.js
+++ services/company.service.js
@@ -68,25 +68,26 @@ class CompanyService {
         status: 200,
         message: '업체 생성 성공',
       };
-
+    } catch (err) {
       console.log(err);
       return {
         status: 500,
         message: 'Server error',
       };
     }
+  };
 
-    allGetCompany = async () => {
-      try {
-        const result = await this._companyRepository.allGetCompany();
-        return {
-          status: 200,
-          message: result,
-        };
-      } catch (err) {
-        return { status: 500, message: 'Server Error' };
-      }
-    };
+  allGetCompany = async () => {
+    try {
+      const result = await this._companyRepository.allGetCompany();
+      return {
+        status: 200,
+        message: result,
+      };
+    } catch (err) {
+      return { status: 500, message: 'Server Error' };
+    }
+  };
   putCompany = async (companyId, companyName, time, additional, service, phoneNumber, link, userId, imageUrl, map) => {
     try {
       const isUser = await this._companyRepository.isUser(userId);
@@ -137,66 +138,65 @@ class CompanyService {
         };
       }
       const result = await this._companyRepository.putCompany(companyId, companyName, time, additional, service, phoneNumber, link, imageUrl, map);
-        if (!result) {
-          return {
-            status: 400,
-            message: '업체 수정 실패',
-          };
-        }
+      if (!result) {
         return {
-          status: 200,
-          message: '업체 수정 성공',
+          status: 400,
+          message: '업체 수정 실패',
         };
-      } catch (err) {
-        console.log(err);
-        return { status: 500, message: 'Server Error' };
       }
-    };
-    deleteCompany = async (companyId, userId) => {
-      try {
-        const isUser = await this._companyRepository.isUser(userId);
-        if (isUser.type === 'user' || isUser.type === 'trainer') {
-          return {
-            status: 401,
-            message: '헬스장의 정보를 수정 할 권한이 없습니다',
-          };
-        }
-        const isMe = await this._companyRepository.isMe(companyId);
-        if (isUser.type === 'owner' && isMe.userId != userId) {
-          return {
-            status: 401,
-            message: '자신의 헬스장 정보만 수정할 수 있습니다',
-          };
-        }
-        if (!companyId) {
-          return {
-            status: 400,
-            message: '업체 ID 미입력',
-          };
-        }
-        const company = await this._companyRepository.oneGetCompany(companyId);
-        if (!company) {
-          return {
-            status: 400,
-            message: '존재하지 않는 업체 ID',
-          };
-        }
-        const result = await this._companyRepository.deleteCompany(companyId);
-        if (!result) {
-          return {
-            status: 400,
-            message: '업체 삭제 실패',
-          };
-        }
+      return {
+        status: 200,
+        message: '업체 수정 성공',
+      };
+    } catch (err) {
+      console.log(err);
+      return { status: 500, message: 'Server Error' };
+    }
+  };
+  deleteCompany = async (companyId, userId) => {
+    try {
+      const isUser = await this._companyRepository.isUser(userId);
+      if (isUser.type === 'user' || isUser.type === 'trainer') {
         return {
-          status: 200,
-          message: '업체 삭제 성공',
+          status: 401,
+          message: '헬스장의 정보를 수정 할 권한이 없습니다',
         };
-      } catch (err) {
-        console.log(err);
-        return { status: 500, message: 'Server Error' };
       }
-    };
+      const isMe = await this._companyRepository.isMe(companyId);
+      if (isUser.type === 'owner' && isMe.userId != userId) {
+        return {
+          status: 401,
+          message: '자신의 헬스장 정보만 수정할 수 있습니다',
+        };
+      }
+      if (!companyId) {
+        return {
+          status: 400,
+          message: '업체 ID 미입력',
+        };
+      }
+      const company = await this._companyRepository.oneGetCompany(companyId);
+      if (!company) {
+        return {
+          status: 400,
+          message: '존재하지 않는 업체 ID',
+        };
+      }
+      const result = await this._companyRepository.deleteCompany(companyId);
+      if (!result) {
+        return {
+          status: 400,
+          message: '업체 삭제 실패',
+        };
+      }
+      return {
+        status: 200,
+        message: '업체 삭제 성공',
+      };
+    } catch (err) {
+      console.log(err);
+      return { status: 500, message: 'Server Error' };
+    }
   };
 }
 
